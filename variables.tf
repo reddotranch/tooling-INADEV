9cd3186f31c4b68b3ee53494a6c98513e12d634d
TDW <mbwork1@yahoo.com>
2025-01-20 09:14:22 -0500

    updates

--- variables.tf
+++ variables.tf
@@ -33,17 +33,17 @@ variable "subnet_id" {
 ##########################
 #TF state file
 ##########################
-#variable "s3_bucket" {
-#  description = "The name of the S3 bucket for storing Terraform state"
-#  type        = string
-#  default     = "east-inadev-tfstate"
-#}
+variable "s3_bucket" {
+  description = "The name of the S3 bucket for storing Terraform state"
+  type        = string
+  default     = "east-inadev-tfstate"
+}
 
-#variable "dynamodb_table" {
-#  description = "The name of the DynamoDB table for state locking"
-#  type        = string
-#  default     = "terraform-state-lock-table"
-#}
+variable "dynamodb_table" {
+  description = "The name of the DynamoDB table for state locking"
+  type        = string
+  default     = "terraform-state-lock-table"
+}
 
 variable "main-region" {
   description = "The AWS region to deploy resources"
