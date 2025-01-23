9a28dab06f4319ee1c80ec6a1fb3482bcb9b5a19
TDW <mbwork1@yahoo.com>
2025-01-22 23:54:02 -0500

    update maven_sonarqube.sh

--- modules/maven-sonarqube-server/maven_sonarqube.sh
+++ modules/maven-sonarqube-server/maven_sonarqube.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 #Installing kubectl client
-curl -O https://s3.us-east-2.amazonaws.com/amazon-eks/1.27.12/2024-04-19/bin/linux/amd64/kubectl
+curl -O https://s3.us-west-2.amazonaws.com/amazon-eks/1.27.16/2024-12-12/bin/linux/amd64/kubectl
 chmod +x ./kubectl
 mkdir -p $HOME/bin && cp ./kubectl $HOME/bin/kubectl && export PATH=$HOME/bin:$PATH
 
